493c7a1749d849b6c92cd51663a4c00515dd8365
Mauricio Alarcon <mauricio.alarcon@coxautoinc.com>
2022-06-27 21:15:33 -0400

    added policy evaluation test

--- errors/IAM.ts
+++ errors/IAM.ts
@@ -6,3 +6,13 @@ export const PolicyStatementNotFound: (policy: string, policyStatement: PolicySt
         code: PolicyStatementNotFound.name,
         message: `${policy} does not contain statement ${JSON.stringify(policyStatement)}`
     })
+export const PolicyEvaluationFailed: (policy: string, expectedAction: string) => ErrorDescription =
+    (policy, expectedAction) => ({
+        code: PolicyEvaluationFailed.name,
+        message: `${expectedAction} fails when running against ${policy}`
+    })
+export const PolicyEvaluationSuccededError: (policy: string, expectedAction: string) => ErrorDescription =
+    (policy, expectedAction) => ({
+        code: PolicyEvaluationSuccededError.name,
+        message: `${expectedAction} on succeded when running against ${policy}. It was expected to fail`
+    })
--- tests/IAM.ts
+++ tests/IAM.ts
@@ -1,10 +1,15 @@
 import { AttributeEquality, CatchTestError, Test } from ".";
 import { TestError } from "../errors";
-import { PolicyStatementNotFound } from "../errors/IAM";
+import { PolicyEvaluationFailed, PolicyEvaluationSuccededError, PolicyStatementNotFound } from "../errors/IAM";
 import { ManagedPolicy } from "../resources/IAM/Policy";
 import { Role } from "../resources/IAM/Role";
 import { AWSPolicyDocument, PolicyStatement } from "../types/IAM";
 import { TestResult } from "../types/tests";
+import {
+    SimulateCustomPolicyCommand,
+    SimulateCustomPolicyCommandInput,
+    IAMClient
+} from "@aws-sdk/client-iam";
 
 export class RoleDataPropertiesTest extends AttributeEquality {
     defaultAttributes: string[] = [
@@ -23,20 +28,38 @@ export class RoleDataPropertiesTest extends AttributeEquality {
     }
 }
 
-interface CheckPolicyStatementsParameters {
-    resource: any
-    policyDocument: string
-    statements: PolicyStatement[]
-}
-export class CheckPolicyStatements extends Test {
+export abstract class PolicyStatementsTest extends Test {
     policyDocumentEncoded: string
     policyDocument: AWSPolicyDocument | undefined
     expectationStatements: PolicyStatement[]
-    constructor(testParameters: CheckPolicyStatementsParameters) {
+    constructor(testParameters: PolicyStatementsTestParameters) {
         super()
         this.resource = testParameters.resource
         this.policyDocumentEncoded = testParameters.policyDocument
         this.expectationStatements = testParameters.statements
+    }
+    preProcessStatements() {
+        this.expectationStatements = this.expectationStatements.map(statement => ({
+            ...statement,
+            Action: this.returnArray(statement.Action),
+            Resource: this.returnArray(statement.Resource)
+        }))
+        this.policyDocument = JSON.parse(decodeURIComponent(this.policyDocumentEncoded))
+    }
+    returnArray(strOrArray: string | string[]): string[] {
+        if (typeof strOrArray === "string") strOrArray = [strOrArray]
+        return strOrArray
+    }
+}
+
+interface PolicyStatementsTestParameters {
+    resource: any
+    policyDocument: string
+    statements: PolicyStatement[]
+}
+export class CheckPolicyStatements extends PolicyStatementsTest {
+    constructor(testParameters: PolicyStatementsTestParameters) {
+        super(testParameters)        
     }
     compareStatements(statement: PolicyStatement) {
         return this.policyDocument?.Statement.find(docStatement => {
@@ -65,6 +88,7 @@ export class CheckPolicyStatements extends Test {
     @CatchTestError()
     async run(): Promise<TestResult> {
         this.checkLoadOutput()
+        this.preProcessStatements()
         this.policyDocument = JSON.parse(decodeURIComponent(this.policyDocumentEncoded))
         this.expectationStatements.forEach(statement => {
             if (!this.compareStatements(statement)) throw new TestError(PolicyStatementNotFound(this.resourceName, statement))
@@ -84,5 +108,50 @@ export class CheckManagedPolicyStatements extends CheckPolicyStatements {
             statements: policy.policyExpectations?.PolicyDocumentStatements || []
         })
         this.resourceName = policy.name || ManagedPolicy.name
+    }
+}
+
+export class EvaluatePolicyDocument extends PolicyStatementsTest {
+    constructor(testParameters: PolicyStatementsTestParameters) {
+        super(testParameters)
+    }
+    @CatchTestError()
+    async run(): Promise<TestResult> {
+        this.checkLoadOutput()
+        this.preProcessStatements()
+        const iamClient: IAMClient = this.resource.environment.getAWSClient("iam")
+        this.policyDocument = JSON.parse(decodeURIComponent(this.policyDocumentEncoded))
+
+        const requests = this.expectationStatements.map(({Action, Resource, Effect}) => {
+            let params: SimulateCustomPolicyCommandInput = {
+                ActionNames: Action as string[],
+                PolicyInputList: [decodeURIComponent(JSON.stringify(this.policyDocument))],
+                ResourceArns: Resource as string[]
+            }
+            
+            return iamClient.send(new SimulateCustomPolicyCommand(params))
+        })
+        const requestResponses = await Promise.all(requests)
+        const mappedResponses = requestResponses.map(({EvaluationResults}) => {
+            let mappedEvaluation:{[key: string]: string} = {}
+            EvaluationResults?.forEach(({EvalActionName, EvalDecision}) => {
+                mappedEvaluation[EvalActionName || ""] = EvalDecision || ""
+            })
+            return mappedEvaluation
+        })
+        this.expectationStatements.forEach(({Action, Effect}, index) => {
+            let evaluationObj = mappedResponses[index]
+            ;(Action as string[]).forEach((action) => {
+                if (evaluationObj[action] === "allowed" && Effect === "Deny") throw new TestError(PolicyEvaluationSuccededError(this.resourceName, action))
+                if (evaluationObj[action] !== "allowed" && Effect === "Allow") throw new TestError(PolicyEvaluationFailed(this.resourceName, action))
+            })
+            
+        })
+        
+        return {
+            success: true,
+            message: "All statements pass the evaluation"
+        }
+        
     }
 }
